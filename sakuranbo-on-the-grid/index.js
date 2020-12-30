7f84fd55481671de1c0943fd9649bd63dbef377d
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-12-30 19:57:18 +0900

    refactor: Separate into functions

--- sakuranbo-on-the-grid/index.js
+++ sakuranbo-on-the-grid/index.js
@@ -1,73 +1,200 @@
-(() => {
-  const containerGridEl = document.querySelector(".container");
-  const TEXT = "SAKURANBO";
+let VIEW = "mobile";
+let TEXT = "SAKURANBO";
+let ROW_COUNT = 8;
+let COL_COUNT = 8;
+let LENGTH = `${100 / COL_COUNT}vw`;
 
-  let ROW = 8;
-  let COL = 8;
+const setConfigForMobile = () => {
+  ROW_COUNT = 8;
+  COL_COUNT = 8;
+  LENGTH = `${100 / COL_COUNT}vw`;
+};
 
-  if (window.innerWidth >= 1280) {
-    ROW = 8;
-    COL = 12;
+const setConfigForTablet = () => {
+  ROW_COUNT = 8;
+  COL_COUNT = 8;
+  LENGTH = "64px";
+  VIEW = "tablet";
+};
+
+const setConfigForDesktop = () => {
+  ROW_COUNT = 8;
+  COL_COUNT = 12;
+  LENGTH = "88px";
+};
+
+const getRootEl = () => document.querySelector("#root");
+
+const getContainerEl = () => document.querySelector(".container");
+
+const getFirstGridItem = () => {
+  const containerEl = getContainerEl();
+  return containerEl.firstChild;
+};
+
+const getGridItemEls = () => {
+  const containerEl = getContainerEl();
+  return Array.from(containerEl.children);
+};
+
+const getCharacters = () => {
+  const totalCount = ROW_COUNT * COL_COUNT;
+  const characters = TEXT.repeat(Math.ceil(totalCount / TEXT.length))
+    .slice(0, totalCount)
+    .split("");
+
+  return characters;
+};
+
+const getGridItemRow = (index) => Math.floor(index / COL_COUNT) + 1;
+const getGridItemCol = (index) => (index % COL_COUNT) + 1;
+const getGridItemMatrix = (index) => {
+  const row = getGridItemRow(index);
+  const col = getGridItemCol(index);
+  return { row, col };
+};
+
+const getDistanceBetweenAB = (pointA, pointB) => {
+  const { rowA, colA } = pointA;
+  const { rowB, colB } = pointB;
+  return Math.sqrt(
+    Math.abs(Math.pow(rowA - rowB, 2)) + Math.abs(Math.pow(colA - colB, 2))
+  );
+};
+
+const getTransformScale = (distance, weighting = 1) => {
+  if (distance === 0) {
+    return 1 * weighting;
   }
 
-  let length = `${100 / COL}vw)`;
-  if (window.innerWidth >= 768) {
-    length = "64px";
+  return (1 / distance) * weighting;
+};
+
+const updateGridItemElByScale = (gridItemEl, scale) => {
+  const gridItemTextEl = gridItemEl.firstChild;
+
+  let fontWeight = 400;
+  if (scale >= 0.3) {
+    fontWeight = 500;
+  }
+  if (scale >= 0.5) {
+    fontWeight = 700;
   }
 
-  if (window.innerWidth >= 1280) {
-    length = "88px";
-  }
+  gridItemTextEl.dataset.scale = scale;
+  gridItemTextEl.style.transform = `scale(${scale})`;
+  gridItemTextEl.style.color = `hsl(${321 + 15 * scale}deg 100% 49%)`;
+  gridItemTextEl.style["font-weight"] = fontWeight;
+};
 
-  containerGridEl.style["grid-template-columns"] = `repeat(${COL}, ${length}`;
+const renderContainerEl = () => {
+  const containerEl = document.createElement("div");
+  containerEl.setAttribute("class", "container");
+  containerEl.setAttribute(
+    "style",
+    `
+    grid-template-rows: repeat(${ROW_COUNT}, ${LENGTH});
+    grid-template-columns: repeat(${COL_COUNT}, ${LENGTH});
+    `
+  );
+  return containerEl;
+};
 
-  const gridItemEls = TEXT.repeat(20)
-    .slice(0, ROW * COL)
-    .split("")
-    .map((character, i) => {
-      const row = Math.floor(i / COL) + 1;
-      const col = (i % COL) + 1;
-      return `<div class="grid-item" data-row="${row}" data-col="${col}"><span class="grid-item-text">${character}</span></div>`;
-    })
-    .join("");
-  containerGridEl.innerHTML = gridItemEls;
+const renderGridItemEl = (row, col, character) => {
+  const gridItemEl = document.createElement("div");
 
-  const containerEl = document.querySelector(".container");
-  Array.from(containerEl.children).forEach((gridItemEl) => {
-    gridItemEl.addEventListener("mouseover", (ev) => {
-      const targetGridItemEl = ev.currentTarget;
-      const { row: targetRow, col: targetCol } = targetGridItemEl.dataset;
+  gridItemEl.setAttribute("class", "grid-item");
+  gridItemEl.setAttribute("data-row", row);
+  gridItemEl.setAttribute("data-col", col);
 
-      Array.from(containerEl.children).forEach((gridItemEl) => {
-        const gridItemTextEl = gridItemEl.firstChild;
-        const { row, col } = gridItemEl.dataset;
+  gridItemEl.addEventListener("mouseover", handleGridItemMouseover);
 
-        const scaleValue =
-          gridItemEl === targetGridItemEl
-            ? 1
-            : 1 /
-              Math.sqrt(
-                Math.abs(Math.pow(targetRow - row, 2)) +
-                  Math.abs(Math.pow(targetCol - col, 2))
-              );
+  const gridItemTextEl = renderGridItemTextEl(character);
+  gridItemEl.append(gridItemTextEl);
 
-        let fontWeight = 400;
-        if (scaleValue >= 0.3) {
-          fontWeight = 500;
-        }
-        if (scaleValue >= 0.5) {
-          fontWeight = 700;
-        }
+  return gridItemEl;
+};
 
-        gridItemTextEl.style.transform = `scale(${scaleValue})`;
-        gridItemTextEl.style.color = `hsl(${
-          321 + 15 * scaleValue
-        }deg 100% 49%)`;
+const handleGridItemMouseover = (ev) => {
+  const targetGridItemEl = ev.currentTarget;
 
-        gridItemTextEl.style["font-weight"] = fontWeight;
-      });
-    });
+  const gridItemEls = getGridItemEls();
+  gridItemEls.forEach((gridItemEl) => {
+    const { row, col } = gridItemEl.dataset;
+    const { row: targetRow, col: targetCol } = targetGridItemEl.dataset;
+    const distance = getDistanceBetweenAB(
+      { rowA: row, colA: col },
+      { rowB: targetRow, colB: targetCol }
+    );
+    const scale = getTransformScale(distance, 1.25);
+    updateGridItemElByScale(gridItemEl, scale);
+  });
+};
+
+const renderGridItemTextEl = (character) => {
+  const gridItemTextEl = document.createElement("span");
+  gridItemTextEl.setAttribute("class", "grid-item-text");
+  gridItemTextEl.append(character);
+
+  return gridItemTextEl;
+};
+
+const triggerInitialEvent = () => {
+  const mouseoverEvent = new Event("mouseover");
+  const firstGridItem = getFirstGridItem();
+  firstGridItem.dispatchEvent(mouseoverEvent);
+};
+
+const render = () => {
+  const rootEl = getRootEl();
+  rootEl.innerHTML = "";
+  const containerEl = renderContainerEl();
+  rootEl.append(containerEl);
+
+  const characters = getCharacters();
+  characters.forEach((character, i) => {
+    const { row, col } = getGridItemMatrix(i, COL_COUNT);
+    const gridItemEl = renderGridItemEl(row, col, character);
+    containerEl.append(gridItemEl);
   });
 
-  containerGridEl.firstChild.dispatchEvent(new Event("mouseover"));
-})();
+  triggerInitialEvent();
+};
+
+window.addEventListener("load", () => {
+  if (window.innerWidth >= 1280) {
+    setConfigForDesktop();
+  } else if (window.innerWidth >= 768) {
+    setConfigForTablet();
+  } else {
+    setConfigForMobile();
+  }
+
+  render();
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1280 && VIEW !== "desktop") {
+    setConfigForDesktop();
+    VIEW = "desktop";
+    render();
+    return;
+  }
+
+  if (
+    window.innerWidth >= 768 &&
+    window.innerWidth < 1280 &&
+    VIEW !== "tablet"
+  ) {
+    setConfigForTablet();
+    render();
+    return;
+  }
+
+  if (window.innerWidth < 768 && VIEW !== "mobile") {
+    setConfigForMobile();
+    VIEW = "mobile";
+    render();
+    return;
+  }
+});
